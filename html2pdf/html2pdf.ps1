c548a43a47ff58359ba26eeef98f7aee050fb844
Axel Bons <axel.bons@yoobi.nl>
2022-10-19 11:07:20 +0200

    html2pdf

---
+++ html2pdf/html2pdf.ps1
@@ -0,0 +1,1 @@
+c:\dev\wkhtmltopdf\bin\wkhtmltopdf --page-height 297mm --page-width 210mm -B 0 -R 0 -T 0 -L 0 --dpi 96 --encoding UTF-8 --quiet --no-stop-slow-scripts a.html a.pdf
---
+++ html2pdf/readme.md
@@ -0,0 +1,3 @@
+download https://wkhtmltopdf.org/
+naar c:\dev\wkhtmltopdf
+vervolgens kan je de .ps1 uitvoeren om van a.html een a.pdf te maken
